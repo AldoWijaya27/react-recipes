95d70a79f2a7825941532725188f2eca44f56ce1
AldoWijaya27 <aldo.wijaya2012@students.unila.ac.id>
2023-03-25 01:27:50 +0700

    commit

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -1,18 +1,21 @@
-import './Recipes.css';
+import "./Recipes.css";
 
-function Recipes({label, calories, ingredientLines, image}) {
-    return(
-        <div>
-            <h2>{label}</h2>
-            <img src={image} alt='food' className="recipe-image"/>
-            <p>{calories.toFixed()} calories</p>
-            <ul className='recipe-list'>
-                {ingredientLines.map((ingredient, index) => (
-                    <li key={index} className='recipe-list-item'>{ingredient}</li>
-                ))}
-            </ul>
-        </div>
-    )
+function Recipes({ label, calories, ingredientLines, image, type }) {
+  return (
+    <div className="main-content">
+      <h3>{label}</h3>
+      <p className="type">{type}</p>
+      <img src={image} alt="food" className="recipe-image" />
+      <p>{calories.toFixed()} calories</p>
+      <ul className="recipe-list">
+        {ingredientLines.map((ingredient, index) => (
+          <li key={index} className="recipe-list-item">
+            {ingredient}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Recipes;
